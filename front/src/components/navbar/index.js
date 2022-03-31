2de81303cfb1ebaf9d402db705280e756cd27e23
Igor Martins <ipdmartins@gmail.com>
2022-03-31 00:42:20 -0400

    feat: homepage and navbar

--- front/src/components/navbar/index.js
+++ front/src/components/navbar/index.js
@@ -3,15 +3,15 @@ import {
   Collapse,
   Navbar,
   NavbarToggler,
-  NavbarBrand,
   Nav,
   NavItem,
   NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Input,
+  Button
 } from 'reactstrap';
 
 const Headers = (props) => {
@@ -21,40 +21,38 @@ const Headers = (props) => {
 
   return (
     <div>
-      <Navbar color="primary" dark expand="md">
-        <NavbarBrand href="/">Iot UQAC</NavbarBrand>
+      <Card>
+        <CardBody className="text-center bg-info text-dark">
+          <CardTitle tag="h3">
+            Outils de visualisation de la collecte et du traitement des données
+          </CardTitle>
+          <CardSubtitle className="mb-2" tag="h6">
+            Par Basile TALMON LARODERIE, Igor MARTINS, Mamadou DIALLO, Vincent DOLLO
+          </CardSubtitle>
+          <CardSubtitle tag="h6">
+            Dans le cadre du cours 8INF912: Sécurité IoT
+          </CardSubtitle>
+        </CardBody>
+      </Card>
+      <Navbar color="secondary" className="text-dark" light expand="md">
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem>
-              <NavLink>Object 1</NavLink>
+              <NavLink>Présentation du projet</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink>Object 2</NavLink>
+              <NavLink>Trouver un objet par catégorie</NavLink>
             </NavItem>
-
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
+            <NavItem className="float-right">
+              <Input placeholder="Rechercher un objet par son nome" />
+            </NavItem>
+            <Button type="submit" color="primary"><span className='bi bi-search'></span> Search</Button>
           </Nav>
-          <NavbarText>Simple Text</NavbarText>
         </Collapse>
       </Navbar>
-    </div>
+    </div >
+
   );
 }
 
