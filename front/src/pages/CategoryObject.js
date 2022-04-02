429fc6cfd4bf44f5b7899808e3de6bf0e8e410ec
Igor Martins <ipdmartins@gmail.com>
2022-04-01 22:11:17 -0400

    fix: directory structure

--- front/src/pages/CategoryObject.js
+++ front/src/pages/CategoryObject.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Navbar from '../components/navbar/index';
 import Footbar from '../components/footer/index';
 import { Card, CardBody, CardTitle, CardText, Row, Col } from 'reactstrap';
-import logoHome from '../components/images/home.png';    
-import logoMedical from '../components/images/medical.png';    
-import logoTransport from '../components/images/transport.png';    
+import logoHome from '../assets/images/home.png'    
+import logoMedical from '../assets/images/medical.png';    
+import logoTransport from '../assets/images/transport.png';    
 
 
 export function CategoryObject(){
