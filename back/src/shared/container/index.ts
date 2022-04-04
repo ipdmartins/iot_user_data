febc3524972f7f8660faf5ce93fcc8f688a09834
Igor <ipdmartins@gmail.com>
2022-04-04 01:30:42 -0400

    fix: back strucute

--- back/src/shared/container/index.ts
+++
@@ -1,7 +0,0 @@
-import { container } from 'tsyringe';
-import { ObjectRepository } from '../../modules/data/repositories/implementations/ObjectRepository';
-import { IObjectRepository } from '../../modules/data/repositories/IObjectRepository';
-
-container.registerSingleton<IObjectRepository>(
-    'ObjectRepository', ObjectRepository
-);
